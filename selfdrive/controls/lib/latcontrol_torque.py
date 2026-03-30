755b8147c431e3671107e471db06cb3ec1ee1254
Uncle-Tony <211085818+Uncle-Tony@users.noreply.github.com>
2026-03-30 14:12:18 -0500

    latcontrol_torque: freeze integrator below 2 m/s (was 5)

--- selfdrive/controls/lib/latcontrol_torque.py
+++ selfdrive/controls/lib/latcontrol_torque.py
@@ -117,7 +117,7 @@ class LatControlTorque(LatControl):
       # do error correction in lateral acceleration space, convert at end to handle non-linear torque responses correctly
       pid_log.error = float(error * kp_working)
 
-      freeze_integrator = steer_limited_by_safety or CS.steeringPressed or CS.vEgo < 5
+      freeze_integrator = steer_limited_by_safety or CS.steeringPressed or CS.vEgo < 2
       output_lataccel = self.pid.update(pid_log.error, speed=CS.vEgo, feedforward=ff, freeze_integrator=freeze_integrator)
       output_torque = self.torque_from_lateral_accel(output_lataccel, self.torque_params)
 
